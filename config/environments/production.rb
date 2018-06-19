6a0be2239cdc4a77fd3c33bbd80b9764fc1c74bd
Matt Smith <msmith@missiondata.com>
2018-06-18 20:26:47 -0400

    Email Prod settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,8 +62,17 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "private-events_#{Rails.env}"
-
+  config.action_mailer.default_url_options = { :host => 'frozen-earth-98979.herokuapp.com' }
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "email-smtp.us-east-1.amazonaws.com",
+    :port => 587,
+    :user_name => ENV["SES_SMTP_USERNAME"], #Your SMTP user
+    :password => ENV["SES_SMTP_PASSWORD"], #Your SMTP password
+    :authentication => :login,
+    :enable_starttls_auto => true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
